fd3d8d40ad5c13a269351f26cd1aa676f8091a21
Talisson Oliveira <talissonlux@outlook.com>
2022-01-14 03:00:12 -0300

    feat: create home section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Header } from '../components/Header'
+import { CgSearch } from 'react-icons/cg'
 
+import styles from './home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +13,23 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <main>
+        <section className={styles.hero}>
+          <div className={styles.heroContent}>
+            <h1>Ready for Trying a new recipe?</h1>
+
+            <div className={styles.search}>
+              <input type="text" placeholder="Search healthy recipes" />
+              <button type="button">
+                <CgSearch />
+              </button>
+            </div>
+          </div>
+
+          <img src="/images/illustration.svg" alt="Illustration" />
+        </section>
+      </main>
     </>
   )
 }
