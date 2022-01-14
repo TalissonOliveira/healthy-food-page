94d812ec439a25a78db828dc4dcdc6711751ad38
Talisson Oliveira <talissonlux@outlook.com>
2022-01-13 22:05:48 -0300

    feat: initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Healthy Food | Home</title>
+      </Head>
+
+      <h1>Healthy Food</h1>
+    </>
+  )
+}
+
+export default Home
